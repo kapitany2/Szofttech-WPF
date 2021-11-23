0b52373a9378e3506276c011e580c1b2b33e6acf
Csaba <w3x3f2@hallgato.uniduna.hu>
2021-11-23 14:06:03 +0100

    CellStatus enum létrehozása

---
+++ Szofttech-WPF/Logic/CellStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szofttech_WPF.Logic
+{
+    public enum CellStatus
+    {
+        Empty,
+        EmptyHit,
+        NearShip,
+        Ship,
+        ShipHit,
+        ShipSunk
+    }
+}
