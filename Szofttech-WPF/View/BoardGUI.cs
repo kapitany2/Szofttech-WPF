27d14ae7444f33622766854d9f97f8e1a73d6e40
Csaba <w3x3f2@hallgato.uniduna.hu>
2021-11-24 14:45:04 +0100

    Hibajavítás

---
+++ Szofttech-WPF/View/BoardGUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Szofttech_WPF.View
+{
+    public class BoardGUI : UserControl
+    {
+        public BoardGUI()
+        {
+        }
+    }
+}
