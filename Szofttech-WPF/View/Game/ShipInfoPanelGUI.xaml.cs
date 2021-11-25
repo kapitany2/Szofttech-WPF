6345f482333de0ac4842dd6bf7925e3dce860f9b
Csaba <w3x3f2@hallgato.uniduna.hu>
2021-11-25 12:55:17 +0100

    ShipInfoPanelGUI megvalósítás

--- Szofttech-WPF/View/Game/ShipInfoPanelGUI.xaml.cs
+++ Szofttech-WPF/View/Game/ShipInfoPanelGUI.xaml.cs
@@ -20,9 +20,67 @@ namespace Szofttech_WPF.View.Game
     /// </summary>
     public partial class ShipInfoPanelGUI : UserControl
     {
-        public ShipInfoPanelGUI()
+        private Label felirat;
+        private readonly int shipSize;
+        private int piece;
+        public ShipInfoPanelGUI(int shipSize, int piece)
         {
             InitializeComponent();
+            Background = new SolidColorBrush(Color.FromRgb(66, 121, 184));
+            this.shipSize = shipSize;
+            this.piece = piece;
+            felirat = new Label();
+            felirat.Content = "1x" + shipSize + ": " + piece + "db";
+            felirat.HorizontalAlignment = HorizontalAlignment.Center;
+            window.Children.Add(felirat);
+        }
+
+        public void SetPiece(int piece)
+        {
+            this.piece = piece;
+            felirat.Content = "1x" + shipSize + ": " + piece + "db";
+            if (piece > 0)
+            {
+                IsEnabled = true;
+            }
+            else
+            {
+                IsEnabled = false;
+                UnSelect();
+            }
+        }
+
+        public int getPiece()
+        {
+            return this.piece;
+        }
+
+        public void Select()
+        {
+            Background = new SolidColorBrush(Color.FromRgb(34, 89, 152));
+        }
+
+        public void UnSelect()
+        {
+            Background = new SolidColorBrush(Color.FromRgb(66, 121, 184));
+        }
+
+        public void decrease()
+        {
+            piece--;
+            felirat.Content = "1x" + shipSize + ": " + piece + "db";
+            if (piece == 0)
+            {
+                IsEnabled = false;
+                UnSelect();
+            }
+        }
+
+        public void increase()
+        {
+            piece++;
+            felirat.Content = "1x" + shipSize + ": " + piece + "db";
+            IsEnabled = true;
         }
     }
 }
