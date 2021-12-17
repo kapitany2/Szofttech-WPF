3ee13ea300149d4171b163cc13a6f057759a575b
CDZR0 <vilmosmalarik@gmail.com>
2021-12-17 16:22:54 +0100

    rendes IP a váróképernyőn

--- Szofttech-WPF/View/GameGUI.xaml.cs
+++ Szofttech-WPF/View/GameGUI.xaml.cs
@@ -33,7 +33,7 @@ namespace Szofttech_WPF.View
         public GameGUI(int port) : this(Settings.getIP(), port)
         {
             Server = new Server(port);
-            ip.Text = String.Format("Your game host on: {0}:{1}", Settings.getIP(),Settings.getPort());
+            ip.Text = String.Format("Host Address: {0}:{1}", Server.getLocalIP(),Settings.getPort());
             ip.Visibility = Visibility.Visible;
         }
 
