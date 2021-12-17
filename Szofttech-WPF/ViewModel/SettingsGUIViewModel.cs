f4ee4c3814917108b8e430dec434bac788f2101c
CDZR0 <vilmosmalarik@gmail.com>
2021-12-17 16:35:52 +0100

    nem használt importok kivéve

--- Szofttech-WPF/ViewModel/SettingsGUIViewModel.cs
+++ Szofttech-WPF/ViewModel/SettingsGUIViewModel.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using System.Timers;
-using System.Windows;
 using Szofttech_WPF.Utils;
 using Szofttech_WPF.ViewModel.Base;
 
