0541033351385a767e827283acb353b88cd6139b
kapitany2 <nycs007@gmail.com>
2021-12-19 20:12:04 +0100

    chat szövegszín témához állítva

--- Szofttech-WPF/ViewModel/ChatViewModel.cs
+++ Szofttech-WPF/ViewModel/ChatViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Windows.Media;
 using Szofttech_WPF.EventArguments.Chat;
+using Szofttech_WPF.Utils;
 using Szofttech_WPF.ViewModel.Base;
 
 namespace Szofttech_WPF.ViewModel
@@ -7,7 +9,8 @@ namespace Szofttech_WPF.ViewModel
     public class ChatViewModel : BaseViewModel
     {
         string chatMessages, chatInput;
-
+        private SolidColorBrush foregroundColor;
+        public SolidColorBrush ForegroundColor { get => foregroundColor; set { foregroundColor = value; OnPropertyChanged(); } }
         public RelayCommand SendCommand { get; }
         public string ChatMessages { get => chatMessages; set { chatMessages = value; OnPropertyChanged(); } }
         public string ChatInput { get => chatInput; set { chatInput = value; OnPropertyChanged(); } }
@@ -15,6 +18,10 @@ namespace Szofttech_WPF.ViewModel
         public ChatViewModel()
         {
             SendCommand = new RelayCommand(send);
+            if (Settings.getBackgroundColor() == Color.FromRgb(37, 57, 66))
+                foregroundColor = new SolidColorBrush(Color.FromRgb(52, 235, 174));
+            else
+                foregroundColor = new SolidColorBrush(Colors.Black);
         }
 
         private void send()
