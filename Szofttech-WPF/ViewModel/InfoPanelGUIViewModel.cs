966bd618af6104c8cf27ca2695423817312c6394
KissDániel <kiss.daniel2000@gmail.com>
2021-12-19 19:07:55 +0100

    új képek, új ViewModel

---
+++ Szofttech-WPF/ViewModel/InfoPanelGUIViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Szofttech_WPF.ViewModel.Base;
+
+namespace Szofttech_WPF.ViewModel
+{
+    internal class InfoPanelGUIViewModel : BaseViewModel
+    {
+        private bool isVisible;
+
+        public bool IsVisible { get => isVisible; set { isVisible = value; OnPropertyChanged(); } }
+
+        private bool isNotVisible;
+
+        public bool IsNotVisible { get => isNotVisible; set { isNotVisible = value; OnPropertyChanged(); } }
+
+        public void changeVisibility(bool enabled)
+        {
+            isVisible = enabled;
+            isNotVisible = !enabled;
+        }
+    }
+}
