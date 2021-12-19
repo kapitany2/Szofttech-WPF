741f19530e393770898a4b4e766a381215d534e1
KissDániel <kiss.daniel2000@gmail.com>
2021-12-19 19:38:41 +0100

    megint

--- Szofttech-WPF/View/Game/InfoPanelGUI.xaml.cs
+++ Szofttech-WPF/View/Game/InfoPanelGUI.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Szofttech_WPF.ViewModel;
 
 namespace Szofttech_WPF.View.Game
 {
@@ -23,6 +24,7 @@ namespace Szofttech_WPF.View.Game
         public InfoPanelGUI()
         {
             InitializeComponent();
+            DataContext = new InfoPanelGUIViewModel();
         }
     }
 }
