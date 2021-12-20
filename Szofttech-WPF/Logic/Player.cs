61ad36e1b6b62be173698097262320a2b8b42550
kapitany2 <nycs007@gmail.com>
2021-12-20 21:30:39 +0100

    kibővítés

--- Szofttech-WPF/Logic/Player.cs
+++ Szofttech-WPF/Logic/Player.cs
@@ -1,19 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Szofttech_WPF.Logic
+﻿namespace Szofttech_WPF.Logic
 {
     public class Player
     {
         public int Identifier { get; set; }
-        public bool isReady = false;
+        public int Win { get; set; }
+        public int Lose { get; set; }
+        public bool isReady { get; set; } = false;
+        public bool wantRematch { get; set; } = false;
         public Board Board { get; set; }
 
         public Player()
         {
+            Board = new Board();
+        }
+
+        public void Reset()
+        {
+            isReady = false;
+            wantRematch = false;
             Board = new Board();
         }
     }
