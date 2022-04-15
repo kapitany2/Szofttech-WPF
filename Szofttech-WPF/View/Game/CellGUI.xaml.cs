e27286b05d106ced9faadba16aca13878bd48dbe
kapitany2 <nycs007@gmail.com>
2022-04-15 17:44:11 +0200

    rajzok képekkel, fölösleges dolgok törölve

--- Szofttech-WPF/View/Game/CellGUI.xaml.cs
+++ Szofttech-WPF/View/Game/CellGUI.xaml.cs
@@ -13,11 +13,6 @@ namespace Szofttech_WPF.View.Game
     /// </summary>
     public partial class CellGUI : UserControl
     {
-        //private readonly Color BackGroundColor = Color.FromRgb(82, 137, 200);
-        //private readonly Color shipColor = Color.FromRgb(18, 73, 136);
-        private Color BackGroundColor;
-        private ImageBrush BackGroundImageBrush;
-        private Color shipColor;
         public readonly int I;
         public readonly int J;
         public bool isHorizontal { get; private set; }
@@ -29,18 +24,9 @@ namespace Szofttech_WPF.View.Game
         public CellGUI(int i, int j)
         {
             InitializeComponent();
-            //BackGroundColor = ColorChanger.DarkeningColor(Settings.getBackgroundColor(), 32);
-            //if (BackGroundColor.R == 218 && BackGroundColor.G == 249 && BackGroundColor.B == 255)
-            //    shipColor = ColorChanger.DarkeningColor(BackGroundColor, -128);
-            //else
-            //    shipColor = ColorChanger.DarkeningColor(BackGroundColor, -64);
             this.I = i;
             this.J = j;
             CellStatus = CellStatus.Empty;
-            //Background = new SolidColorBrush(BackGroundColor);
-            BackGroundImageBrush = new ImageBrush();
-            BackGroundImageBrush.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/View/Resources/bs_default.png"));
-
         }
         public void setInfo(bool horizontal, int pieces, int counter)
         {
@@ -62,34 +48,13 @@ namespace Szofttech_WPF.View.Game
 
         private void setColorSelected() => Background = new SolidColorBrush(Colors.Orange);
         
-        private void SetColorUnSelected() => Background = BackGroundImageBrush;
+        private void SetColorUnSelected() => Background = null;
 
         public void setCell(CellStatus cell)
         {
             CellStatus = cell;
             Dispatcher.Invoke(() =>
             {
-                //switch (cell)
-                //{
-                //    case CellStatus.Empty:
-                //        Background = new SolidColorBrush(BackGroundColor);
-                //        break;
-                //    case CellStatus.EmptyHit:
-                //        break;
-                //    case CellStatus.NearShip:
-                //        break;
-                //    case CellStatus.Ship:
-                //        Background = new SolidColorBrush(shipColor);
-                //        ChangedToShip?.Invoke(this, EventArgs.Empty);
-                //        break;
-                //    case CellStatus.ShipHit:
-                //        break;
-                //    case CellStatus.ShipSunk:
-                //        ChangedToSunk?.Invoke(this, EventArgs.Empty);
-                //        break;
-                //    default:
-                //        break;
-                //}
                 switch (cell)
                 {
                     case CellStatus.Ship:
@@ -108,42 +73,15 @@ namespace Szofttech_WPF.View.Game
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
-            ImageBrush imageBrush = new ImageBrush();
-            Pen pen;
-            Brush brush;
-            Rect rect;
-            Point xp;
-            Point yp;
-            SolidColorBrush solidColorBrush;
             ImageBrush img;
             switch (CellStatus)
             {
                 case CellStatus.Empty:
-                    //Background = new SolidColorBrush(BackGroundColor);
-                    //Background = null; // Ez valamiért csak így működik! Maradjon null!!
-                    Background = BackGroundImageBrush;
+                    Background = null; // Ez valamiért csak így működik! Maradjon null!!
                     break;
                 case CellStatus.EmptyHit:
-                    //háttér
-                    //solidColorBrush = new SolidColorBrush(BackGroundColor);
-                    //pen = new Pen(solidColorBrush, 1);
-                    //rect = new Rect(0, 0, 30, 30);
-                    //drawingContext.DrawRectangle(solidColorBrush, pen, rect);
-                    ////hullám
-                    //pen = new Pen(Brushes.Blue, 1);
-                    //for (int k = 0; k < 6; k++)
-                    //{
-                    //    int[] x = { 0, 5, 10, 15, 20, 25, 30 };
-                    //    int[] y = { 0 + k * 5, 5 + k * 5, 0 + k * 5, 5 + k * 5, 0 + k * 5, 5 + k * 5, 0 + k * 5 };
-                    //    for (int i = 1; i < x.Length; i++)
-                    //    {
-                    //        drawingContext.DrawLine(pen, new Point(x[i - 1], y[i - 1]), new Point(x[i], y[i]));
-                    //    }
-                    //}
-                    ////hogy látszódjon is...
-                    ////Background = null;
                     img = new ImageBrush();
-                    img.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/View/Resources/bs_water.png"));
+                    img.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/View/Resources/bs_waterhit.png"));
                     Background = img;
                     break;
                 case CellStatus.NearShip:
@@ -168,21 +106,9 @@ namespace Szofttech_WPF.View.Game
                     }
                     break;
                 case CellStatus.ShipHit:
-                    //háttér
-                    solidColorBrush = new SolidColorBrush(shipColor);
-                    pen = new Pen(solidColorBrush, 1);
-                    rect = new Rect(0, 0, 30, 30);
-                    drawingContext.DrawRectangle(solidColorBrush, pen, rect);
-                    //X
-                    pen = new Pen(Brushes.Red, 3);
-                    xp = new Point(0, 0);
-                    yp = new Point(30, 30);
-                    drawingContext.DrawLine(pen, xp, yp);
-                    xp = new Point(0, 30);
-                    yp = new Point(30, 0);
-                    drawingContext.DrawLine(pen, xp, yp);
-                    //hogy látszódjon is...
-                    Background = null;
+                    img = new ImageBrush();
+                    img.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/View/Resources/bs_shiphit.png"));
+                    Background = img;
                     break;
                 case CellStatus.ShipSunk:
                     try
